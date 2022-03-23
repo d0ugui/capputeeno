d7bb1722d455bed7e69fcf50a4416fe267a6082b
Douglas Oliveira <douglaspo_97@outlook.com>
2022-03-23 16:59:05 -0300

    fix: head in product page

--- pages/product/[productItem].js
+++ pages/product/[productItem].js
@@ -1,6 +1,7 @@
 //* Next
 import Link from 'next/link';
 import Image from 'next/image';
+import Head from 'next/head';
 
 //* Icons
 import { FiShoppingBag } from 'react-icons/fi';
@@ -8,60 +9,69 @@ import { GrUndo } from 'react-icons/gr';
 
 //* GraphQL
 import client from '../../src/services/apollo';
-import uniqueProduct from '../../src/graphql/uniqueProduct';
+import getProductByID from '../../src/graphql/getProductByID';
 
 import { Container, Content, ProductDetails } from './styles';
 
 export default function Product({ data }) {
   return (
-    <Container>
-      <Link href="/">
-        <span>
-          <GrUndo size={18} />
-          Voltar
-        </span>
-      </Link>
-      <Content>
-        <Image src={data.Product.image_url} width={640} height={580} />
-        <ProductDetails>
-          <span>{data.Product.category}</span>
-          <h1>{data.Product.name}</h1>
-          <strong>
-            {
+    <>
+      <Head>
+        <title>
+          {data.Product.name}
+          {' '}
+          | capputeeno
+        </title>
+      </Head>
+      <Container>
+        <Link href="/">
+          <span>
+            <GrUndo size={18} />
+            Voltar
+          </span>
+        </Link>
+        <Content>
+          <Image src={data.Product.image_url} width={640} height={580} />
+          <ProductDetails>
+            <span>{data.Product.category}</span>
+            <h1>{data.Product.name}</h1>
+            <strong>
+              {
               (data.Product.price_in_cents / 100).toLocaleString(
                 'pt-BR',
                 { style: 'currency', currency: 'BRL' },
               )
             }
-          </strong>
+            </strong>
 
-          <small>
-            *Frete de R$40,00 para todo o Brasil.
-            Grátis para compras acima de R$900,00.
-          </small>
+            <small>
+              *Frete de R$40,00 para todo o Brasil.
+              Grátis para compras acima de R$900,00.
+            </small>
 
-          <div>
-            <h2>Descrição</h2>
-            <p>
-              {data.Product.description}
-            </p>
-          </div>
-          <button
-            type="button"
-            onClick={() => console.log('Hello')}
-          >
-            <FiShoppingBag />
-            Adicionar ao carrinho
-          </button>
-        </ProductDetails>
-      </Content>
-    </Container>
+            <div>
+              <h2>Descrição</h2>
+              <p>
+                {data.Product.description}
+              </p>
+            </div>
+            <button
+              type="button"
+              onClick={() => console.log('Hello')}
+            >
+              <FiShoppingBag />
+              Adicionar ao carrinho
+            </button>
+          </ProductDetails>
+        </Content>
+      </Container>
+    </>
   );
 }
 
 export async function getServerSideProps({ params }) {
   const { data } = await client.query({
-    query: uniqueProduct, variables: { id: params.productItem },
+    query: getProductByID, variables: { id: params.productItem },
   });
 
   return {
--- src/graphql/getProductByID.js
+++ src/graphql/getProductByID.js
@@ -1,7 +1,7 @@
 import { gql } from '@apollo/client';
 
 export default gql`
-  query AllProducts($id: ID!) {
+  query GET_PRODUCT_BY_ID($id: ID!) {
     Product(id: $id) {
       name
       price_in_cents
